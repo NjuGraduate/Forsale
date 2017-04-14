a73b1e86bc65eba2817ef92e66ededd85b64a536
njuser <865717138@qq.com>
2017-04-14 15:24:43 +0800

    update

---
+++ src/cn/edu/nju/controller/FileUpdateController.java
@@ -0,0 +1,35 @@
+package cn.edu.nju.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileUpdateController {
+	@RequestMapping("/upload.do")
+	public String fileupload(@RequestParam(value="file",required=false) MultipartFile file,HttpServletRequest requst,Model model) {
+		// TODO Auto-generated method stub
+		String path = requst.getSession().getServletContext().getRealPath("/upload");
+		String Name = file.getOriginalFilename();
+//		String extName = old.substring(old.indexOf("."));
+		//requst.getServletPath()
+		File newFile = new File(path,Name);
+		try {
+			file.transferTo(newFile);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return "";
+	}
+	
+}
