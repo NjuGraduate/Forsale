69ed3f588df7ee961439bb5a530fcd725174c4d7
Neal6 <1114102304@qq.com>
2017-05-18 05:52:57 +0800

    购物车和交易记录

--- src/cn/edu/nju/mapper/CartInfoMapper.java
+++ src/cn/edu/nju/mapper/CartInfoMapper.java
@@ -1,5 +1,12 @@
 package cn.edu.nju.mapper;
 
-public interface CartInfoMapper {
+import java.util.List;
+import cn.edu.nju.po.CommodityInfo;
+import cn.edu.nju.po.CartInfo;
+import cn.edu.nju.po.UserInfo;
 
+public interface CartInfoMapper {
+	public boolean addCart(CartInfo re);
+	public boolean removeCart(CartInfo re);
+	public List<CartInfo> getCartByUserAccount(UserInfo user);
 }
