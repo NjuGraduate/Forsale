020b61e5868d1ac6ba1e4f4acc28ae28cbeacf6c
Neal6 <1114102304@qq.com>
2017-05-20 23:52:13 +0800

    GoodsInfo接口

--- src/cn/edu/nju/mapper/GoodsInfoMapper.java
+++ src/cn/edu/nju/mapper/GoodsInfoMapper.java
@@ -1,5 +1,17 @@
 package cn.edu.nju.mapper;
 
+import java.util.List;
+
+import cn.edu.nju.po.GoodsInfo;
+import cn.edu.nju.po.UserInfo;
+
 public interface GoodsInfoMapper {
-	
+	public boolean addGoods(GoodsInfo user);
+	public boolean removeRecord(GoodsInfo co);
+	public boolean updateGoods(GoodsInfo co);
+	public List<GoodsInfo> getGoodsLike(GoodsInfo co);
+	public List<GoodsInfo> getGoods();
+	public GoodsInfo getGoodsById(GoodsInfo co);
+	public GoodsInfo getGoodsByUserAccount(UserInfo user);
+
 }
